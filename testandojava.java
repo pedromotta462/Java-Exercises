107a2fb8abe2a709ddb86d050f6bca97f9c15f12
pedromotta462 <pedromotta1d2015@gmail.com>
2022-05-31 00:25:02 -0300

    first commit

---
+++ testandojava.java
@@ -0,0 +1,18 @@
+ public class testandojava {
+     public static void main(String[] args) {
+        
+        int a;
+        float b;  
+        a=5;
+        b=3.14f;
+        final int JI = 10;
+        //comentario é assim
+        char k = 'b';
+        String v = "pedro motta";
+        boolean n = true;
+        float I = a*b;
+        System.out.println("numero 1: " + I + " numero 2: " + JI + "\n este e o char: " + k); 
+        System.out.println(v + " " + n);
+
+     }
+ }
